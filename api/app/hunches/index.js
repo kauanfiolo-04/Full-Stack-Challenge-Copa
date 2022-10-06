7e478a2ae79b04132f6550439fdd7527e09e866d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-10-06 14:00:50 -0300

    Subindo token, login e criptografia da senha

---
+++ api/app/hunches/index.js
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const create = async ctx => {
+    const userId = 'cl8x4l3580000f5h8fky4vgaq'
+
+    if (!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
+        ctx.status = 400
+        return
+    }
+
+    const { gameId } = ctx.request.body
+    const homeTeamScore = parseInt(ctx.request.body.homeTeamScore)
+    const awayTeamScore = parseInt(ctx.request.body.awayTeamScore)
+
+    try {
+        const [hunch] = await prisma.hunch.findMany({
+            where: { userId, gameId },
+        })
+
+        ctx.body = hunch
+            ? await prisma.hunch.update({
+                where: {
+                    id: hunch.id
+                },
+                data: {
+                    homeTeamScore,
+                    awayTeamScore
+                }
+            })
+            : await prisma.hunch.create({
+                data: {
+                    userId,
+                    gameId,
+                    homeTeamScore,
+                    awayTeamScore
+                }
+            })
+    } catch (error) {
+        console.log(error)
+        ctx.body = error
+        ctx.status = 500
+    }
+}
